4fae96b909c854201294ec2ed83d0447adb5e5ab
Soumil Singh <soumil.sps@gmail.com>
2023-03-27 22:15:21 +0530

    added about and contact pages

--- app.js
+++ app.js
@@ -20,13 +20,36 @@ app.get("/", function(req, res){
     res.render("home.ejs", {postTitle: postTitle, postContent: postContent});
 });
 
+app.get("/about", function(req, res){
+    res.render("about.ejs");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact.ejs");
+});
+
+
+app.get("/post", function(req, res){
+    res.render("post.ejs");
+});
+
 app.post('/compose', function(req, res){
     postTitle.push(req.body.postTitle);
     postContent.push(req.body.postContent);
     res.redirect('/');
 });
 
+app.post('/about', function(req, res){
+    res.redirect('/about');
+});
 
+app.post('/contact', function(req, res){
+    res.redirect('/contact');
+});
+
+app.post("/", function(req, res){
+    res.redirect('/');
+});
 
 app.listen(PORT, function(){
     console.log("Server started on port 3000");
